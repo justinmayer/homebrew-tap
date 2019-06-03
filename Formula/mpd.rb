e322cf1c826764cd4cd44083607c3716a0b3d6c4
Justin Mayer <entroP@gmail.com>
2019-06-03 15:08:15 +0200

    Add formula for Music Player Daemon (MPD)

---
+++ Formula/mpd.rb
@@ -0,0 +1,121 @@
+class Mpd < Formula
+  desc "Music Player Daemon"
+  homepage "https://www.musicpd.org/"
+  url "https://www.musicpd.org/download/mpd/0.21/mpd-0.21.7.tar.xz"
+  sha256 "512062bc529bee04d4ccfaa2c6e720afc85bb071e49850a096b5ed518b3b412c"
+  head "https://github.com/MusicPlayerDaemon/MPD.git"
+
+  # This formula is only for compiling from source, so there are no bottles.
+  bottle :unneeded
+
+  depends_on "boost" => :build
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
+  depends_on "pkg-config" => :build
+  depends_on "expat"
+  depends_on "faad2"
+  depends_on "justinmayer/tap/ffmpeg"
+  depends_on "flac"
+  depends_on "fluid-synth"
+  depends_on "glib"
+  depends_on "icu4c"
+  depends_on "lame"
+  depends_on "libao"
+  depends_on "libgcrypt"
+  depends_on "libid3tag"
+  depends_on "libmpdclient"
+  depends_on "libnfs"
+  depends_on "libsamplerate"
+  depends_on "libupnp"
+  depends_on "libvorbis"
+  depends_on "opus"
+  depends_on "sqlite"
+
+  def install
+    # mpd specifies -std=gnu++0x, but clang appears to try to build
+    # that against libstdc++ anyway, which won't work.
+    # The build is fine with G++.
+    ENV.libcxx
+
+    args = %W[
+      --prefix=#{prefix}
+      --sysconfdir=#{etc}
+      -Dlibwrap=disabled
+      -Dmad=disabled
+      -Dmpcdec=disabled
+      -Dsoundcloud=disabled
+      -Dao=enabled
+      -Dbzip2=enabled
+      -Dexpat=enabled
+      -Dffmpeg=enabled
+      -Dfluidsynth=enabled
+      -Dnfs=enabled
+      -Dupnp=enabled
+      -Dvorbisenc=enabled
+    ]
+
+    system "meson", *args, "output/release", "."
+    system "ninja", "-C", "output/release"
+    ENV.deparallelize # Directories are created in parallel, so let's not do that
+    system "ninja", "-C", "output/release", "install"
+
+    (etc/"mpd").install "doc/mpdconf.example" => "mpd.conf"
+  end
+
+  def caveats; <<~EOS
+    MPD requires a config file to start.
+    Please copy it from #{etc}/mpd/mpd.conf into one of these paths:
+      - ~/.mpd/mpd.conf
+      - ~/.mpdconf
+    and tailor it to your needs.
+  EOS
+  end
+
+  plist_options :manual => "mpd"
+
+  def plist; <<~EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+    <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>WorkingDirectory</key>
+        <string>#{HOMEBREW_PREFIX}</string>
+        <key>ProgramArguments</key>
+        <array>
+            <string>#{opt_bin}/mpd</string>
+            <string>--no-daemon</string>
+        </array>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>KeepAlive</key>
+        <true/>
+        <key>ProcessType</key>
+        <string>Interactive</string>
+    </dict>
+    </plist>
+  EOS
+  end
+
+  test do
+    pid = fork do
+      exec "#{bin}/mpd --stdout --no-daemon --no-config"
+    end
+    sleep 2
+
+    begin
+      ohai "Connect to MPD command (localhost:6600)"
+      TCPSocket.open("localhost", 6600) do |sock|
+        assert_match "OK MPD", sock.gets
+        ohai "Ping server"
+        sock.puts("ping")
+        assert_match "OK", sock.gets
+        sock.close
+      end
+    ensure
+      Process.kill "SIGINT", pid
+      Process.wait pid
+    end
+  end
+end
