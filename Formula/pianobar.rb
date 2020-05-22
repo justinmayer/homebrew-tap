3dc39afe29df9b6638d197f6fe927be3a11737d8
Justin Mayer <entroP@gmail.com>
2020-05-22 08:35:54 +0200

    Add Pianobar formula

---
+++ Formula/pianobar.rb
@@ -0,0 +1,39 @@
+class Pianobar < Formula
+  desc "Command-line player for https://pandora.com"
+  homepage "https://github.com/PromyLOPh/pianobar/"
+  url "https://6xq.net/pianobar/pianobar-2020.04.05.tar.bz2"
+  sha256 "6c173b6b29ccc1f432e0013fb425e8f9cb4261539b58d344e0b2274963726480"
+  head "https://github.com/PromyLOPh/pianobar.git"
+
+  # This formula is only for compiling from source, so there are no bottles.
+  bottle :unneeded
+
+  depends_on "pkg-config" => :build
+  depends_on "justinmayer/tap/ffmpeg"
+  depends_on "json-c"
+  depends_on "libao"
+  depends_on "libgcrypt"
+
+  uses_from_macos "curl"
+
+  def install
+    # Discard Homebrew's CFLAGS as Pianobar reportedly doesn't like them
+    ENV["CFLAGS"] = "-O2 -DNDEBUG " +
+                    # Or it doesn't build at all
+                    "-std=c99 " +
+                    # build if we aren't /usr/local'
+                    "#{ENV.cppflags} #{ENV.ldflags}"
+    system "make", "PREFIX=#{prefix}"
+    system "make", "install", "PREFIX=#{prefix}"
+
+    prefix.install "contrib"
+  end
+
+  test do
+    require "pty"
+    PTY.spawn(bin/"pianobar") do |stdout, stdin, _pid|
+      stdin.putc "\n"
+      assert_match "pianobar (#{version})", stdout.read
+    end
+  end
+end
